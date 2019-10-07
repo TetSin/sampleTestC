d66069178c593a7f92ea22253f0d7fb3ac3254df
andreydorokhin <andrey.dorokhin.dam@gmail.com>
2019-10-07 20:55:39 +0300

    digits

--- src/test/java/AddTwoValuesTests.java
+++ src/test/java/AddTwoValuesTests.java
@@ -28,7 +28,7 @@ public class AddTwoValuesTests {
         assertEquals("-10 + -5 must be -15", -15, calc.add(-10, -5));
     }
     @Test
-    public void testmultyplyValues() { assertEquals("11 * 7 must be 77", 77, calc.multiply(11, 7)); }
+    public void testmultyplyValues() { assertEquals("1* 7 must be 7", 7, calc.multiply(1, 7)); }
 
     //TODO
     //add tests for new methods
